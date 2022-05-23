4129799d813db5997d0cda0407c130b01aabfe6d
symball <sball@hk.palo-it.com>
2022-05-23 15:54:17 +0800

    Big rework to get site builder in state for L&L
    * Base off bulma starter template
    * Reorganise content
    * remove some redundant artifacts
    * add minification
    
    * fix up second page

--- index.ts
+++ index.ts
@@ -1,18 +1,19 @@
-import './style.css'
-import { welcomeBanner } from './utils/config';
-import { fetchData } from './utils/net';
+import { welcomeBanner } from './src/utils/config'
+import { fetchData } from './src/utils/net'
+import { App } from './src/App'
+
+App()
 
 // Add a JS listener to page which only fires when ready
 document.addEventListener("DOMContentLoaded", async () => {
-    
+    console.log('Home page DOM ready')
+
     // Example of simple element manipulation
     const app = document.querySelector<HTMLDivElement>('#welcome-banner')!
-    app.innerHTML = `<h1>${welcomeBanner}</h1>`;
+    app.innerHTML = `<h1>${welcomeBanner}</h1>`
 
     // Fetching remote content
-    const comicImageSource = await fetchData();
+    const comicImageSource = await fetchData()
     const remoteContentDiv = document.querySelector<HTMLDivElement>('#remote-content')!
     remoteContentDiv.innerHTML = `<img src="${comicImageSource}" />`
-});
-
-
+})
